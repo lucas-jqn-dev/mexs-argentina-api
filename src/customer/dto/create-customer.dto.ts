685f690ededc43ad742f4d203e5607d3b6aab042
Lucas Florio <lucasjoa92@gmail.com>
2024-01-24 14:05:41 -0300

    class validator integrated

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,31 +1,93 @@
+import { IsNotEmpty, IsString } from "class-validator"
+
 export class CreateCustomerDto {
+    @IsString()
+    @IsNotEmpty()
     Nombre: string
+
+    @IsString()
+    @IsNotEmpty()
     Apellido: string
+
+    @IsString()
+    @IsNotEmpty()
     Email: string
+
+    @IsString()
+    @IsNotEmpty()
     Numero: string
-    Calle: string
-    Piso: string
-    Departamento: string
-    Distrito: string
-    Poblacion: string
-    Region: string
-    CodigoPostal: string
-    TelFijo: string
-    TelMovil: string
-    Contacto: string
-    Latitud: string
-    Longitud: string
+
+    @IsString()
+    @IsNotEmpty()
     FechaNacimiento: string
+
+    @IsString()
+    @IsNotEmpty()
     Genero: string
+    
+    @IsString()
+    @IsNotEmpty()
+    Calle: string
+
+    @IsString()
+    Piso: string
+
+    @IsString()
+    Departamento: string
+
+    @IsString()
+    Distrito: string
+
+    @IsString()
+    Poblacion: string
+
+    @IsString()
+    Region: string
+
+    @IsString()
+    CodigoPostal: string
+
+    @IsString()
+    TelFijo: string
+
+    @IsString()
+    TelMovil: string
+
+    @IsString()
+    Contacto: string
+
+    @IsString()
+    Latitud: string
+
+    @IsString()
+    Longitud: string
+
+    @IsString()
+
     //Revisar
     IdStcdt: string
+
+    @IsString()
     Stcd1: string
+
+    @IsString()
     IdSegmentoCli: string
+
+    @IsString()
     Stkzn: string
+
+    @IsString()
     IdFityp: string
+
+    @IsString()
     Vkorg: string
+
+    @IsString()
     Vtweg: string
+
+    @IsString()
     IdOficVta: string
+    
     mensajexcliente: []
     percepcionesxcliente: []
 }
--- src/test-run/dto/get-form.dto.ts
+++ src/test-run/dto/get-form.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from "class-validator"
+
 export class GetFormDto {
+    @IsString()
+    @IsNotEmpty()
     TipoDocumento: string
+
+    @IsString()
+    @IsNotEmpty()
     Documento: string
 }
